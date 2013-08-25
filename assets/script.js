d77dadd91fcdc7a0e558e2fba97847defff67ae1
pizzamx <pizzamx@gmail.com>
2013-08-25 22:57:25 +0800

    init

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// $Id: script.js,v 1.1.8.1 2009/06/19 02:36:38 shannonlucas Exp $
+
+/**
+ * @file script.js
+ * General JavaScript functions for the theme. A default file is required to
+ * get Drupal to automatically load jQuery.
+ */
